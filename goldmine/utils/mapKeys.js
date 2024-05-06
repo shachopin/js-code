448d7e790df429de9f7efef1c09712b46c75dd93
Glitch (dawei-dai-codes) <none>
2024-05-06 04:12:53 +0000

    📹🌔 Updated with Glitch

--- goldmine/utils/mapKeys.js
+++ goldmine/utils/mapKeys.js
@@ -1,7 +1,7 @@
 export const deepMapKeys = (obj, fn) =>
   Array.isArray(obj)
     ? obj.map(val => deepMapKeys(val, fn))
-    : typeof obj === 'object'
+    : obj !== null && typeof obj === 'object'
     ? Object.keys(obj).reduce((acc, current) => {
         const key = fn(current);
         const val = obj[current];
